b0903a74586ae4e5203b7fd0aa06b1dda5e37ed8
DESKTOP-VLPQ0GP\gooha <jssq2468@naver.com>
2022-07-15 21:02:38 +0900

    feat : mainContet 반응형 레이아웃 CSS 적용

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,11 +1,35 @@
 import React from "react";
 import MainBg from "./MainBg";
 import Input from "./Input";
+import { Item } from "./Style/MainPageCSS";
 function MainPage() {
   return (
     <div>
       <MainBg />
       <Input />
+      <Item>
+        {[1, 2, 3, 4, 5, 6].map((element, idx) => {
+          return (
+            <div className="box" key={idx}>
+              <p className="title">{element}</p>
+              <button>코딩테스트</button>
+              <button>서울</button>
+              <button>모집중</button>
+              <hr />
+              <div className="info">
+                <div className="avatar_user">
+                  <img style={{ width: "30px" }} src="/user.png"></img>
+                  <span>noah</span>
+                </div>
+                <div className="avatar_Bookmark">
+                  {" "}
+                  <img style={{ width: "30px" }} src="/ribon.png"></img>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Item>
     </div>
   );
 }
--- src/Components/Style/MainPageCSS.js
+++ src/Components/Style/MainPageCSS.js
@@ -1,5 +1,73 @@
 import styled from "@emotion/styled";
 
-const MainBg = styled.div``;
+const Item = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  background-color: #f0f0f0;
 
-export { MainBg };
+  .box {
+    max-width: 1100px;
+    background-color: white;
+    margin: 20px 20px 20px 20px;
+    cursor: pointer;
+    transition: 0.2s ease-in;
+    padding: 10px;
+    border: solid 1px #f0f0f0;
+    border-radius: 5px;
+    width: 300px;
+    transition: all 0.2s ease-in;
+    box-shadow: 0px 19px 38px rgba(0, 0, 0, 0.03),
+      0px 5px 10px rgba(0, 0, 0, 0.1);
+    &:hover {
+      transform: scale(1.04);
+    }
+    .title {
+      font-size: 1.2em;
+      margin: 15px 0 100px 0;
+      font-weight: bold;
+    }
+    button {
+      font-size: 0.5em;
+      border: solid 1px #f0f0f0;
+      border-radius: 10px;
+      padding: 5px;
+      color: #474646;
+      margin: 0 5px;
+      background-color: #e3e3e3;
+    }
+    .info {
+      display: flex;
+      font-size: 18px;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 10px;
+      .avatar_user {
+        display: flex;
+        align-items: center;
+        margin-left: 5px;
+        width: 36px;
+        height: 36px;
+        span {
+          margin-left: 10px;
+          font-weight: 700;
+        }
+      }
+
+      .avatar_Bookmark {
+        display: flex;
+        align-items: center;
+        width: 30px;
+        height: 30px;
+      }
+    }
+    @media screen and (max-width: 780px) {
+      padding: 68px 40px 27px 30px;
+      height: 400px;
+      width: 100%;
+    }
+  }
+`;
+
+export { Item };
