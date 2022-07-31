50cef3391e25f8ac07618a7da0a9757faf52de8a
이지영 <jssq2468@naver.com>
2022-07-31 16:41:35 +0900

    feat : text editor

--- src/Component/Post/Upload.js
+++ src/Component/Post/Upload.js
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 import { Editor } from "../../Style/UploadCSS";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ReactHtmlParser from "html-react-parser";
 function Upload() {
   const ListRegion = ["서울", "인천", "대구", "부산"];
   const ListField = ["apple", "banana", "grape", "orange"];
-  const ListRecuritment = ["1명","2명","3명","4명","5명","6명","7명","8명","제한없음"]
+  const ListRecuritment = [
+    "1명",
+    "2명",
+    "3명",
+    "4명",
+    "5명",
+    "6명",
+    "7명",
+    "8명",
+    "제한없음",
+  ];
   const [SelectedRegion, setSelected] = useState("");
   const [SelectedField, setSelectedField] = useState("");
-  const [SelectedRecruitment,setSelectedRecruitment] =useState("");
+  const [SelectedRecruitment, setSelectedRecruitment] = useState("");
 
   const handleSelectRegion = (e) => {
     setSelected(e.target.value);
@@ -16,18 +27,16 @@ function Upload() {
   const handleSelectField = (e) => {
     setSelectedField(e.target.value);
   };
-  const handleSelectRecruitment= (e) => {
+  const handleSelectRecruitment = (e) => {
     setSelectedRecruitment(e.target.value);
   };
-
-
-
 
   const [movieContent, setMovieContent] = useState({
     title: "",
     content: "",
   });
 
+  const [viewContent, setViewContent] = useState([]);
   const getValue = (e) => {
     const { name, value } = e.target;
     setMovieContent({
@@ -38,42 +47,49 @@ function Upload() {
   };
   return (
     <Editor>
-
-<div className="selected">
-      <h2>지역</h2>
-      <div className="slectedOption">
-        <select onChange={handleSelectRegion} value={SelectedRegion}>
-          {ListRegion.map((item) => (
-            <option value={item} key={item}>
-              {item}
-            </option>
-          ))}
-        </select>
+      <div className="selected">
+        <h2>지역</h2>
+        <div className="slectedOption">
+          <select onChange={handleSelectRegion} value={SelectedRegion}>
+            {ListRegion.map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <h2>분야</h2>
+        <div className="slectedOption">
+          <select onChange={handleSelectField} value={SelectedField}>
+            {ListField.map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <h2>모집 인원</h2>
+        <div className="slectedOption">
+          <select
+            onChange={handleSelectRecruitment}
+            value={SelectedRecruitment}
+          >
+            {ListRecuritment.map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <h2>분야</h2>
-      <div className="slectedOption">
-        <select onChange={handleSelectField} value={SelectedField}>
-          {ListField.map((item) => (
-            <option value={item} key={item}>
-              {item}
-            </option>
-          ))}
-        </select>
+      <div>
+        {viewContent.map((element) => (
+          <div>
+            <h2>{element.title}</h2>
+            <div>{ReactHtmlParser(element.content)}</div>
+          </div>
+        ))}
       </div>
-      <h2>모집 인원</h2>
-      <div className="slectedOption">
-        <select onChange={handleSelectRecruitment} value={SelectedRecruitment}>
-          {ListRecuritment.map((item) => (
-            <option value={item} key={item}>
-              {item}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
-
-
-    
       <div className="form-wrapper">
         <input
           className="title-input"
@@ -105,11 +121,16 @@ function Upload() {
             console.log("Focus.", editor);
           }}
         />
-        <div className="submit-button" >
-        <button>입력</button>
+
+        <button
+          className="submit-botton"
+          onClick={() => {
+            setViewContent(viewContent.concat({ ...movieContent }));
+          }}
+        >
+          입력
+        </button>
       </div>
-      </div>
-      
     </Editor>
   );
 }
